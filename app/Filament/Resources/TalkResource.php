54e9c984eaf48a242d6a0c1a51a37556dc446b05
jivemachine <the.jeremy.cobb@gmail.com>
2024-10-16 19:55:13 -0500

    table actions

--- app/Filament/Resources/TalkResource.php
+++ app/Filament/Resources/TalkResource.php
@@ -7,14 +7,18 @@ use App\Models\Talk;
 use Filament\Tables;
 use Filament\Forms\Form;
 use App\Enums\TalkLength;
+use App\Enums\TalkStatus;
 use Filament\Tables\Table;
 use Illuminate\Support\Str;
 use Filament\Resources\Resource;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Filters\TernaryFilter;
+use Illuminate\Database\Eloquent\Collection;
 use App\Filament\Resources\TalkResource\Pages;
+use Filament\Tables\Actions\RestoreBulkAction;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\TalkResource\RelationManagers;
 
@@ -63,7 +67,7 @@ class TalkResource extends Resource
                     ->circular()
                     ->defaultImageUrl(function ($record) {
                         return 'https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=' . urlencode($record->speaker->name);
-                }),
+                    }),
                 // Tables\Columns\TextColumn::make('abstract')
                 //     ->wrap(),
                 Tables\Columns\TextColumn::make('speaker.name')
@@ -74,17 +78,17 @@ class TalkResource extends Resource
                 TextColumn::make('status')->badge()
                     ->badge()
                     ->sortable()
-                ->color(function ($state) {
-                    return $state->getColor();
-                }),
+                    ->color(function ($state) {
+                        return $state->getColor();
+                    }),
                 IconColumn::make('length')
-                ->icon(function ($state) {
-                    return match($state) {
-                        TalkLength::NORMAL  => 'heroicon-o-megaphone',
-                        TalkLength::LIGHTNING => 'heroicon-o-bolt',
-                        TalkLength::KEYNOTE => 'heroicon-o-key',
-                    };
-                }),
+                    ->icon(function ($state) {
+                        return match ($state) {
+                            TalkLength::NORMAL  => 'heroicon-o-megaphone',
+                            TalkLength::LIGHTNING => 'heroicon-o-bolt',
+                            TalkLength::KEYNOTE => 'heroicon-o-key',
+                        };
+                    }),
 
             ])
             ->filters([
@@ -97,20 +101,69 @@ class TalkResource extends Resource
                 Tables\Filters\Filter::make('has_avatar')
                     ->label('Show Only Speakers With Avatars')
                     ->toggle()
-                ->query(function ($query) {
-                    return $query->whereHas('speaker', function (Builder $query) {
-                        $query->whereNotNull('avatar');
-                    });
-                }),
+                    ->query(function ($query) {
+                        return $query->whereHas('speaker', function (Builder $query) {
+                            $query->whereNotNull('avatar');
+                        });
+                    }),
 
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->slideOver(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\Action::make('approve')
+                        ->visible(function ($record) {
+                            return $record->status === (TalkStatus::SUBMITTED);
+                        })
+                        ->icon('heroicon-o-check-circle')
+                        ->color('success')
+                        ->action(function (Talk $record) {
+                            $record->approve();
+                        })->after(function () {
+                            Notification::make()->success()->title('This talk was approved')
+                                ->duration(1000)
+                                ->body('The speaker has been notified and the talk has bneem added top the schedule.')
+                                ->send();
+                        }),
+                    Tables\Actions\Action::make('reject')
+                        ->visible(function ($record) {
+                            return $record->status === (TalkStatus::SUBMITTED);
+                        })
+                        ->icon('heroicon-o-no-symbol')
+                        ->color('danger')
+                        ->requiresConfirmation()
+                        ->action(function (Talk $record) {
+                            $record->reject();
+                        })->after(function () {
+                            Notification::make()->danger()->title('This talk was rejected')
+                                ->duration(1000)
+                                ->body('The speaker has been notified.')
+                                ->send();
+                        }),
+                ]),
+
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\BulkAction::make('apprive')
+                    ->action(function (Collection $records) {
+                        $records->each->approve();
+                    }),
+
+
                     Tables\Actions\DeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
                 ]),
+            ])
+            ->headerActions([
+                Tables\Actions\Action::make('export')
+                    ->tooltip('thism will export all filtered records.')
+                    ->action(function ($livewire) {
+                        dd($livewire->getFilteredTableQuery()->count());
+                    })
+
             ]);
     }
 
@@ -126,7 +179,8 @@ class TalkResource extends Resource
         return [
             'index' => Pages\ListTalks::route('/'),
             'create' => Pages\CreateTalk::route('/create'),
-            'edit' => Pages\EditTalk::route('/{record}/edit'),
+            // commenting out turns into modal
+            // 'edit' => Pages\EditTalk::route('/{record}/edit'),
         ];
     }
 }
